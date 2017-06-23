02a427d63aa69825aae29fc716bd99c9236c7b12
Michaël Zasso <targos@protonmail.com>
2017-06-23 17:06:30 +0200

    eslint-fix

--- src/index.js
+++ src/index.js
@@ -3,26 +3,26 @@ import euclideanDistance from 'ml-distance-euclidean';
 
 export default class KNN {
     constructor(dataset, labels, options) {
-            const {
-                distance = euclideanDistance,
-                k = dataset[0].length + 1
-            } = options;
+        const {
+            distance = euclideanDistance,
+            k = dataset[0].length + 1
+        } = options;
 
-            const classes = new Set(labels).size;
+        const classes = new Set(labels).size;
 
-            const points = new Array(dataset.length);
-            for (var i = 0; i < points.length; ++i) {
-                points[i] = dataset[i].slice();
-            }
+        const points = new Array(dataset.length);
+        for (var i = 0; i < points.length; ++i) {
+            points[i] = dataset[i].slice();
+        }
 
-            for (i = 0; i < labels.length; ++i) {
-                points[i].push(labels[i]);
-            }
+        for (i = 0; i < labels.length; ++i) {
+            points[i].push(labels[i]);
+        }
 
-            this.kdTree = new KDTree(points, distance);
-            this.k = k;
-            this.classes = classes;
-            this.distance = distance;
+        this.kdTree = new KDTree(points, distance);
+        this.k = k;
+        this.classes = classes;
+        this.distance = distance;
     }
 
     predict(dataset) {
